b56bd77b228cee427b0e03cd13e3831373c95eae
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-02 00:41:50 +0545

    add card component for home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Carousel from "./Carousel";
+import HomeCard from "./HomeCard";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Home = () => {
-  return <div className="">home</div>;
+  return (
+    <>
+      <div className="relative min-h-screen w-full overflow-hidden bg-gradient-to-b from-teal-100 via-teal-50 to-white">
+        <div className="absolute z-10 flex cursor-pointer items-center justify-center rounded-full hover:text-neutral-600 md:top-[38%] md:left-[8%] lg:top-[42%] lg:left-[9%] xl:top-[45%]">
+          <ChevronLeft />
+        </div>
+        <Carousel />
+        <div className="absolute z-10 flex cursor-pointer items-center justify-center rounded-full hover:text-neutral-600 md:top-[38%] md:right-[8%] lg:top-[42%] lg:right-[9%] xl:top-[45%]">
+          <ChevronRight />
+        </div>
+        <HomeCard />
+      </div>
+    </>
+  );
 };
 
 export default Home;
