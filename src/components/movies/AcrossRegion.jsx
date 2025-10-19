d2e6d5c9f1d8f943df640aeb7c163dfdd206695f
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-19 23:04:41 +0545

    shift the movies api fetching logic to appcontext

--- src/components/movies/AcrossRegion.jsx
+++ src/components/movies/AcrossRegion.jsx
@@ -1,46 +1,17 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import MovieCard from "../movieComponents/MovieCard";
-// import { CurrentMovies } from "../../assets/ApiData";
 
-const AcrossRegion = () => {
-  const [CurrentMovies, setCurrentMovies] = useState([]);
-  async function fetchMovies() {
-    const Current = [];
-    await axios
-      .get(`http://localhost:3000/api/shows/now_playing?region=IN&page=1`)
-      .then((resp) => {
-        Current.push(...resp.data.results);
-      });
-    await axios
-      .get(`http://localhost:3000/api/shows/now_playing?region=NP&page=1`)
-      .then((resp) => {
-        resp.data.results.forEach((movie) => {
-          if (!Current.find((m) => m._id === movie._id)) {
-            Current.push(movie);
-          }
-        });
-        setCurrentMovies(Current);
-      });
-  }
-  // useEffect(() => {
-  //   fetchMovies();
-  // }, []);
-  return (
-    <div>
-      <h2 className="px-2 py-6 font-heading text-lg font-semibold text-neutral-800 lg:text-2xl xl:py-8">
-        Stories Across Region
-      </h2>
-      <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
-        {CurrentMovies.map(
-          (movie) =>
-            movie.poster && (
-              <MovieCard key={movie._id} movie={movie} type="View Details" />
-            ),
-        )}
-      </div>
+const AcrossRegion = ({ movies }) => (
+  <>
+    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
+      {movies.map(
+        (movie) =>
+          movie.poster && (
+            <MovieCard key={movie._id} movie={movie} type="View Details" />
+          ),
+      )}
     </div>
-  );
-};
+  </>
+);
 
 export default AcrossRegion;
--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
+import AppContext from "../../context/AppContext";
 import AcrossRegion from "./AcrossRegion";
 import OnlyInTheatre from "../theatre/OnlyInTheatre";
 import Upcoming from "./Upcoming";
+import Loader from "../loader/Loader";
 
 const Movies = () => {
+  const { isLoading, acrossRegionMovies, onlyInTheatreMovies, upcomingMovies } =
+    useContext(AppContext);
+
+  if (isLoading) return <Loader />;
+
   return (
-    <div className="space-y-8 px-4 py-20 md:px-16 lg:px-32 xl:px-58">
-      <AcrossRegion />
-      {OnlyInTheatre && (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { duration: 0.3 },
+      }}
+      className="min-h-screen space-y-8 px-4 py-20 md:px-16 lg:px-32 xl:px-58"
+    >
+      {acrossRegionMovies.length > 0 && (
+        <div>
+          <h2 className="px-2 py-6 font-heading text-lg font-semibold text-neutral-800 lg:text-2xl xl:py-8">
+            Stories Across Region
+          </h2>
+          <AcrossRegion movies={acrossRegionMovies} />
+        </div>
+      )}
+
+      {onlyInTheatreMovies.length > 0 && (
         <div>
           <h2 className="px-2 py-6 font-heading text-lg font-semibold text-neutral-800 lg:text-2xl xl:py-8">
             Only In Theatre
           </h2>
-          <OnlyInTheatre />
+          <OnlyInTheatre movies={onlyInTheatreMovies} />
         </div>
       )}
-      <Upcoming />
-    </div>
+
+      {upcomingMovies.length > 0 && (
+        <div>
+          <h2 className="px-2 py-6 font-heading text-lg font-semibold text-neutral-800 lg:text-2xl xl:py-8">
+            Upcomings
+          </h2>
+          <Upcoming movies={upcomingMovies} />
+        </div>
+      )}
+    </motion.div>
   );
 };
 
