bb0f610f54325cbb3228e3f6ed9546e91e643215
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-19 23:01:44 +0545

    add and update theatre's all components

--- src/components/theatre/OnlyInTheatre.jsx
+++ src/components/theatre/OnlyInTheatre.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import AppContext from "../../context/AppContext";
 import MovieCard from "../movieComponents/MovieCard";
-import movieDatas from "../../assets/data";
 
 const OnlyInTheatre = () => {
+  const { onlyInTheatreMovies } = useContext(AppContext);
+  const [activeMovieId, setActiveMovieId] = useState(null);
+
   return (
     <div>
       <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
-        {movieDatas.map((movie) => (
-          <MovieCard key={movie._id} movie={movie} type="Buy Tickets" />
+        {onlyInTheatreMovies.map((movie) => (
+          <MovieCard
+            key={movie._id}
+            movie={movie}
+            type="Buy Tickets"
+            activeMovieId={activeMovieId}
+            setActiveMovieId={setActiveMovieId}
+          />
         ))}
       </div>
     </div>
--- src/components/theatre/Theatre.jsx
+++ src/components/theatre/Theatre.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { motion } from "framer-motion";
 import OnlyInTheatre from "./OnlyInTheatre";
 const Theatre = () => {
   return (
-    <div className="min-h-screen px-4 py-20 md:px-16 lg:px-32 xl:px-58">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          duration: 0.4,
+        },
+      }}
+      className="min-h-screen px-4 py-20 md:px-16 lg:px-32 xl:px-58"
+    >
       <h2 className="px-2 py-6 font-heading text-lg font-semibold text-neutral-800 lg:text-2xl xl:py-8">
         Now Showing
       </h2>
       <OnlyInTheatre />
-    </div>
+    </motion.div>
   );
 };
 
