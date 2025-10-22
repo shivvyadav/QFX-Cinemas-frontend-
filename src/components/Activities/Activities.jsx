c4c52a934db4261fdd66d72cb37af46f7d9f2c3f
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-22 21:42:13 +0545

    add the activities page

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-
 const Favourite = () => {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen px-4 py-20 md:px-16 lg:px-32 xl:px-58">
+      <h1 className="text-center font-heading leading-18 text-neutral-800 md:text-base md:leading-24">
+        No Activities Available
+      </h1>
+    </div>
+  );
 };
 
 export default Favourite;
