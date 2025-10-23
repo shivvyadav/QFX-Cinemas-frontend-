ff4ec5db3fe09d07d8d27385116aa9ec3e70c8c8
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-23 23:22:32 +0545

    add the overlay on both small and large devices

--- src/components/movies/Upcoming.jsx
+++ src/components/movies/Upcoming.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "../movieComponents/MovieCard";
 
-const Upcoming = ({ movies }) => (
-  <>
-    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
-      {movies.map(
-        (movie) =>
-          movie.poster && (
-            <MovieCard key={movie._id} movie={movie} type="View Details" />
-          ),
-      )}
-    </div>
-  </>
-);
+const Upcoming = ({ movies }) => {
+  const [activeMovieId, setActiveMovieId] = useState(null);
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
+        {movies.map(
+          (movie) =>
+            movie.poster && (
+              <MovieCard
+                key={movie._id}
+                movie={movie}
+                type="View Details"
+                activeMovieId={activeMovieId}
+                setActiveMovieId={setActiveMovieId}
+              />
+            ),
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Upcoming;
