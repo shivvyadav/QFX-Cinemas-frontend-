bd353d2ef90c44aed63e81737e85605ffdf3f88b
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-23 23:19:13 +0545

    update the component

--- src/components/movieComponents/BuyMovie.jsx
+++ src/components/movieComponents/BuyMovie.jsx
@@ -4,7 +4,7 @@ import AppContext from "../../context/AppContext"; // ✅ import context
 import ShowTimes from "./ShowTimes";
 import Hero from "./Hero";
 import AboutMovie from "./AboutMovie";
-
+import { motion } from "motion/react";
 const BuyMovie = () => {
   const { id } = useParams();
   const { selectedShow, fetchShowDetail } = useContext(AppContext);
@@ -18,11 +18,15 @@ const BuyMovie = () => {
   }
 
   return (
-    <div className="space-y-8 px-4 py-24 md:px-16 md:pt-32 lg:px-32 xl:px-58">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.3 } }}
+      className="space-y-8 px-4 py-24 md:px-16 md:pt-32 lg:px-32 xl:px-58"
+    >
       <Hero movie={selectedShow} />
       <ShowTimes movie={selectedShow} />
       <AboutMovie movie={selectedShow} />
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/movieComponents/ViewMovie.jsx
+++ src/components/movieComponents/ViewMovie.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import AppContext from "../../context/AppContext"; // ✅ import context
+import AppContext from "../../context/AppContext";
 import Hero from "./Hero";
 import AboutMovie from "./AboutMovie";
+import { motion } from "motion/react";
 
 const ViewMovie = () => {
   const { id } = useParams();
@@ -17,10 +18,14 @@ const ViewMovie = () => {
   }
 
   return (
-    <div className="space-y-8 px-4 py-24 md:px-16 md:pt-32 lg:px-32 xl:px-58">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.3 } }}
+      className="space-y-8 px-4 py-24 md:px-16 md:pt-32 lg:px-32 xl:px-58"
+    >
       <Hero movie={selectedMovie} />
       <AboutMovie movie={selectedMovie} />
-    </div>
+    </motion.div>
   );
 };
 
