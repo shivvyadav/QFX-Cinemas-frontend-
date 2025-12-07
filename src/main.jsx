d6960c17357e80c6b40648469c907373c1e44367
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-07 22:52:31 +0545

    update this file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import "./index.css";
 import Routes from "./Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/clerk-react";
+import React, { Suspense } from "react";
+import { AppProvider } from "./context/AppContext.jsx";
+import Loader from "./components/loader/Loader";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,24 +15,34 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <Toaster
-      position="top-center"
-      reverseOrder={false}
-      toastOptions={{
-        success: {
-          style: {
-            background: "white",
-            color: "green",
+    <AppProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            style: {
+              background: "white",
+              color: "green",
+            },
           },
-        },
-        error: {
-          style: {
-            background: "white",
-            color: "red",
+          error: {
+            style: {
+              background: "white",
+              color: "red",
+            },
           },
-        },
-      }}
-    />
-    <Routes />
+        }}
+      />
+      <Suspense
+        fallback={
+          <div className="flex h-screen items-center justify-center">
+            <Loader z={50} />
+          </div>
+        }
+      >
+        <Routes />
+      </Suspense>
+    </AppProvider>
   </ClerkProvider>,
 );
