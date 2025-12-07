f741d868354466d093d20e40123f9dec298b144a
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-07 22:53:03 +0545

    add lazy import

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,131 +1,86 @@
-import React from "react";
+import React, { lazy } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import { AppProvider } from "./context/AppContext.jsx";
 import ProtectedAdminRoute from "./routes/ProtectedAdminRoute";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./components/home/Home";
-import Movies from "./components/movies/Movies";
-import Theatre from "./components/theatre/Theatre";
-import ViewMovie from "./components/movieComponents/ViewMovie";
-import BuyMovie from "./components/movieComponents/BuyMovie";
-import SeatLayout from "./components/seatLayout/SeatLayout";
-import MyBookings from "./components/myBookings/MyBookings";
-import Activities from "./components/Activities/Activities";
-import TicketRate from "./components/more/TicketRate";
-import Gallery from "./components/more/Gallery";
-import Error from "./components/Error";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-import Contact from "./components/Contact";
-import TermsAndCondition from "./components/TermsAndCondition";
-import VerifyPayment from "./components/payment/VerifyPayment.jsx";
 
-// admin imports
-import Nav from "./admin/Nav";
-import Sidebar from "./admin/Sidebar";
-import Dashboard from "./admin/Dashboard";
-import AddShow from "./admin/AddShow";
-import ListShows from "./admin/ListShows";
-import ListBookings from "./admin/ListBookings";
+// Lazy loaded pages
+const Home = lazy(() => import("./components/home/Home"));
+const Movies = lazy(() => import("./components/movies/Movies"));
+const Theatre = lazy(() => import("./components/theatre/Theatre"));
+const ViewMovie = lazy(() => import("./components/movieComponents/ViewMovie"));
+const BuyMovie = lazy(() => import("./components/movieComponents/BuyMovie"));
+const SeatLayout = lazy(() => import("./components/seatLayout/SeatLayout"));
+const MyBookings = lazy(() => import("./components/myBookings/MyBookings"));
+const Activities = lazy(() => import("./components/Activities/Activities"));
+const TicketRate = lazy(() => import("./components/more/TicketRate"));
+const Gallery = lazy(() => import("./components/more/Gallery"));
+const Error = lazy(() => import("./components/Error"));
+const PrivacyPolicy = lazy(() => import("./components/PrivacyPolicy"));
+const Contact = lazy(() => import("./components/Contact"));
+const TermsAndCondition = lazy(() => import("./components/TermsAndCondition"));
+const VerifyPayment = lazy(
+  () => import("./components/payment/VerifyPayment.jsx"),
+);
 
-const Layout = () => {
-  return (
-    <>
-      <AppProvider>
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </AppProvider>
-    </>
-  );
-};
-const AdminLayout = () => {
-  return (
-    <>
-      <Nav />
-      <Sidebar />
-      <Outlet />
-    </>
-  );
-};
+// Admin lazy
+const Nav = lazy(() => import("./admin/Nav"));
+const Sidebar = lazy(() => import("./admin/Sidebar"));
+const Dashboard = lazy(() => import("./admin/Dashboard"));
+const AddShow = lazy(() => import("./admin/AddShow"));
+const ListShows = lazy(() => import("./admin/ListShows"));
+const ListBookings = lazy(() => import("./admin/ListBookings"));
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const AdminLayout = () => (
+  <>
+    <Nav />
+    <Sidebar />
+    <Outlet />
+  </>
+);
+
 const Routes = () => {
   const router = createBrowserRouter([
     {
       element: <Layout />,
       children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
+        { path: "/", element: <Home /> },
         {
           path: "/movies",
           children: [
-            {
-              path: "",
-              element: <Movies />,
-            },
-            {
-              path: "details/:id",
-              element: <ViewMovie />,
-            },
+            { path: "", element: <Movies /> },
+            { path: "details/:id", element: <ViewMovie /> },
           ],
         },
         {
           path: "/theatre",
           children: [
-            {
-              path: "",
-              element: <Theatre />,
-            },
-            {
-              path: "buy/:id",
-              element: <BuyMovie />,
-            },
+            { path: "", element: <Theatre /> },
+            { path: "buy/:id", element: <BuyMovie /> },
           ],
         },
-        {
-          path: "/myBookings",
-          element: <MyBookings />,
-        },
-        {
-          path: "/activities",
-          element: <Activities />,
-        },
-        {
-          path: "/ticketRate",
-          element: <TicketRate />,
-        },
-        {
-          path: "/gallery",
-          element: <Gallery />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        {
-          path: "/privacyPolicy",
-          element: <PrivacyPolicy />,
-        },
-        {
-          path: "/termsAndCondition",
-          element: <TermsAndCondition />,
-        },
+        { path: "/myBookings", element: <MyBookings /> },
+        { path: "/activities", element: <Activities /> },
+        { path: "/ticketRate", element: <TicketRate /> },
+        { path: "/gallery", element: <Gallery /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "/privacyPolicy", element: <PrivacyPolicy /> },
+        { path: "/termsAndCondition", element: <TermsAndCondition /> },
       ],
     },
-    {
-      path: "/seatLayout/:movie_id/:date/:time",
-      element: <SeatLayout />,
-    },
-    {
-      path: "/payment/verifyPayment",
-      element: <VerifyPayment />,
-    },
-    {
-      path: "*",
-      element: <Error />,
-    },
+    { path: "/seatLayout/:movie_id/:date/:time", element: <SeatLayout /> },
+    { path: "/payment/verifyPayment", element: <VerifyPayment /> },
+    { path: "*", element: <Error /> },
+
     {
       element: (
         <ProtectedAdminRoute>
@@ -133,22 +88,10 @@ const Routes = () => {
         </ProtectedAdminRoute>
       ),
       children: [
-        {
-          path: "/admin/dashboard",
-          element: <Dashboard />,
-        },
-        {
-          path: "/admin/addShows",
-          element: <AddShow />,
-        },
-        {
-          path: "/admin/listShows",
-          element: <ListShows />,
-        },
-        {
-          path: "/admin/listBookings",
-          element: <ListBookings />,
-        },
+        { path: "/admin/dashboard", element: <Dashboard /> },
+        { path: "/admin/addShows", element: <AddShow /> },
+        { path: "/admin/listShows", element: <ListShows /> },
+        { path: "/admin/listBookings", element: <ListBookings /> },
       ],
     },
   ]);
