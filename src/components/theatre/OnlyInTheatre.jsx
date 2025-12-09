256907598dafd648e0de58dd9403f6dff4900458
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-09 22:12:07 +0545

    update this file

--- src/components/theatre/OnlyInTheatre.jsx
+++ src/components/theatre/OnlyInTheatre.jsx
@@ -4,24 +4,23 @@ import MovieCard from "../movieComponents/MovieCard";
 import Loader from "../loader/Loader";
 
 const OnlyInTheatre = () => {
-  const { onlyInTheatreMovies } = useContext(AppContext);
+  const { isLoading, onlyInTheatreMovies } = useContext(AppContext);
   const [activeMovieId, setActiveMovieId] = useState(null);
 
+  if (isLoading) return <Loader />;
+
   return (
-    <>
-      {onlyInTheatreMovies.length === 0 && <Loader />}
-      <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
-        {onlyInTheatreMovies.map((movie) => (
-          <MovieCard
-            key={movie._id}
-            movie={movie}
-            type="Buy Tickets"
-            activeMovieId={activeMovieId}
-            setActiveMovieId={setActiveMovieId}
-          />
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:gap-6 xl:grid-cols-4 xl:gap-8">
+      {onlyInTheatreMovies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          type="Buy Tickets"
+          activeMovieId={activeMovieId}
+          setActiveMovieId={setActiveMovieId}
+        />
+      ))}
+    </div>
   );
 };
 
